84e375b615597629d800d84d76b5964ca96cf03a
zvdas <rodriguezzude@gmail.com>
2025-03-22 17:19:03 +0530

    update: firebase added to project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDdNN9xAWlvpwdVlWH1njkhcShFacX99GM",
+    authDomain: "meeting-platform-a5f7b.firebaseapp.com",
+    projectId: "meeting-platform-a5f7b",
+    storageBucket: "meeting-platform-a5f7b.firebasestorage.app",
+    messagingSenderId: "685103213534",
+    appId: "1:685103213534:web:3e1d399ce7b257b5cb8b4f",
+    measurementId: "G-01HF7ERVJV",
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,12 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDdNN9xAWlvpwdVlWH1njkhcShFacX99GM",
+    authDomain: "meeting-platform-a5f7b.firebaseapp.com",
+    projectId: "meeting-platform-a5f7b",
+    storageBucket: "meeting-platform-a5f7b.firebasestorage.app",
+    messagingSenderId: "685103213534",
+    appId: "1:685103213534:web:3e1d399ce7b257b5cb8b4f",
+    measurementId: "G-01HF7ERVJV",
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
